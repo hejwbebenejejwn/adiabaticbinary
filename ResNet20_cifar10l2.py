f366cd820287b1f61908ca3f239098784f874987
David <2072224025@qq.com>
2024-03-29 23:21:58 +0800

    res

--- ResNet20_cifar10l2.py
+++ ResNet20_cifar10l2.py
@@ -169,67 +169,67 @@ else:
 
 print("ResNet20, mode "+mode)
 
+with tf.device('/GPU:0'):
+    if(bw==1 and ba==0):
+        model=ResNet20(0,0)
+        optz=tf.keras.optimizers.SGD(learning_rate=.1,momentum=0.9,nesterov=True,clipvalue=3.0)
+        model(cifar10.x_train[:2])
+        model.compile(optimizer=optz,
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+        model.summary()
+        model.evaluate(cifar10.x_val,cifar10.y_val,batch_size=125,verbose=2)
+        model.fit(cifar10.datagen.flow(cifar10.x_train,cifar10.y_train,batch_size=100),epochs=80,validation_data=(cifar10.x_val,cifar10.y_val),verbose=2)
+        np.savez("DResNet20_l2fp1.npz",*(model.get_weights()))
+        optz.learning_rate.assign(1e-2)
+        model.fit(cifar10.datagen.flow(cifar10.x_train,cifar10.y_train,batch_size=100),epochs=40,validation_data=(cifar10.x_val,cifar10.y_val),verbose=2)
+        np.savez("DResNet20_l2fp2.npz",*(model.get_weights()))
+        optz.learning_rate.assign(1e-3)
+        model.fit(cifar10.datagen.flow(cifar10.x_train,cifar10.y_train,batch_size=100),epochs=20,validation_data=(cifar10.x_val,cifar10.y_val),verbose=2)
+        np.savez("DResNet20_l2fp3.npz",*(model.get_weights()))
 
-if(bw==1 and ba==0):
-    model=ResNet20(0,0)
-    optz=tf.keras.optimizers.SGD(learning_rate=.1,momentum=0.9,nesterov=True,clipvalue=3.0)
-    model(cifar10.x_train[:2])
-    model.compile(optimizer=optz,
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-    model.summary()
-    model.evaluate(cifar10.x_val,cifar10.y_val,batch_size=125,verbose=2)
-    model.fit(cifar10.datagen.flow(cifar10.x_train,cifar10.y_train,batch_size=100),epochs=80,validation_data=(cifar10.x_val,cifar10.y_val),verbose=2)
-    np.savez("DResNet20_l2fp1.npz",*(model.get_weights()))
-    optz.learning_rate.assign(1e-2)
-    model.fit(cifar10.datagen.flow(cifar10.x_train,cifar10.y_train,batch_size=100),epochs=40,validation_data=(cifar10.x_val,cifar10.y_val),verbose=2)
-    np.savez("DResNet20_l2fp2.npz",*(model.get_weights()))
-    optz.learning_rate.assign(1e-3)
-    model.fit(cifar10.datagen.flow(cifar10.x_train,cifar10.y_train,batch_size=100),epochs=20,validation_data=(cifar10.x_val,cifar10.y_val),verbose=2)
-    np.savez("DResNet20_l2fp3.npz",*(model.get_weights()))
+        with np.load("DResNet20_l2fp3.npz") as wts:
+            wtt=[wts[nms] for nms in wts.files]
 
-    with np.load("DResNet20_l2fp3.npz") as wts:
-        wtt=[wts[nms] for nms in wts.files]
+        for k in [37,35,33,31,29,27,25,23,21,19,17,15,13,11,9,7,5,3]: #nmk
+            wtt.insert(k,np.array(tf.reduce_max(tf.math.abs(wtt[k-1])).numpy()*1.5))
+        for k in [2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53]: # reset kernel
+            wtt[k]=tf.math.atanh(wtt[k]/tf.reduce_max(tf.math.abs(wtt[k]))/1.5).numpy()
+        for k in [1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55]: # remove bias
+            wtt[k]*=0
+        for k in [56,53,50,47,44,41,38,35,32,29,26,23,20,17,14,11,8,5]: # kk
+            wtt.insert(k,np.array(1.0))
 
-    for k in [37,35,33,31,29,27,25,23,21,19,17,15,13,11,9,7,5,3]: #nmk
-        wtt.insert(k,np.array(tf.reduce_max(tf.math.abs(wtt[k-1])).numpy()*1.5))
-    for k in [2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53]: # reset kernel
-        wtt[k]=tf.math.atanh(wtt[k]/tf.reduce_max(tf.math.abs(wtt[k]))/1.5).numpy()
-    for k in [1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55]: # remove bias
-        wtt[k]*=0
-    for k in [56,53,50,47,44,41,38,35,32,29,26,23,20,17,14,11,8,5]: # kk
-        wtt.insert(k,np.array(1.0))
+        model=ResNet20(1,0)
+        optz=tf.keras.optimizers.SGD(learning_rate=.01,momentum=0.9,nesterov=True,clipvalue=3.0)
+        model(cifar10.x_train[:2])
+        model.compile(optimizer=optz,
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+        model.summary()
+        model.set_weights(wtt)
+        model.evaluate(cifar10.x_val,cifar10.y_val,batch_size=125,verbose=2)
 
-    model=ResNet20(1,0)
-    optz=tf.keras.optimizers.SGD(learning_rate=.01,momentum=0.9,nesterov=True,clipvalue=3.0)
-    model(cifar10.x_train[:2])
-    model.compile(optimizer=optz,
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-    model.summary()
-    model.set_weights(wtt)
-    model.evaluate(cifar10.x_val,cifar10.y_val,batch_size=125,verbose=2)
+        trr=trainer.Trainer(50,"w")
+        trr.prto=1.2
+        trr.maxpush=3
+        trr.target_acc=.9 # higher value also works but is slower
+        trr.lr_power=0
+        trr.train(model,cifar10.datagen,cifar10.x_train,cifar10.y_train,cifar10.x_val,cifar10.y_val,600,1e-2,"DResNet20_l2"+str(rn),True,cifar10.x_test,cifar10.y_test)
 
-    trr=trainer.Trainer(50,"w")
-    trr.prto=1.2
-    trr.maxpush=3
-    trr.target_acc=.9 # higher value also works but is slower
-    trr.lr_power=0
-    trr.train(model,cifar10.datagen,cifar10.x_train,cifar10.y_train,cifar10.x_val,cifar10.y_val,600,1e-2,"DResNet20_l2"+str(rn),True,cifar10.x_test,cifar10.y_test)
+        with np.load("DResNet20_l2"+str(rn) + mode + "_Best.npz") as wts:
+            model.set_weights([wts[nms] for nms in wts.files])
 
-    with np.load("DResNet20_l2"+str(rn) + mode + "_Best.npz") as wts:
-        model.set_weights([wts[nms] for nms in wts.files])
-
-    model.set_kk(1e5)
-    model.evaluate(cifar10.x_val,cifar10.y_val,batch_size=trr.val_bs,verbose=2)
-    model.evaluate(cifar10.x_test,cifar10.y_test,batch_size=trr.val_bs,verbose=2)
-
-    x_trains=cifar10.x_train[:]
-    for _ in range(5):
-        x_trains=x_trains[np.random.permutation(len(x_trains))]
-        for ik in range(0,len(x_trains),trr.val_bs):
-            model(x_trains[ik:ik+trr.val_bs],training=True)
-            if (ik % 2000 == 0):
-                print("#", end="")
+        model.set_kk(1e5)
         model.evaluate(cifar10.x_val,cifar10.y_val,batch_size=trr.val_bs,verbose=2)
         model.evaluate(cifar10.x_test,cifar10.y_test,batch_size=trr.val_bs,verbose=2)
+
+        x_trains=cifar10.x_train[:]
+        for _ in range(5):
+            x_trains=x_trains[np.random.permutation(len(x_trains))]
+            for ik in range(0,len(x_trains),trr.val_bs):
+                model(x_trains[ik:ik+trr.val_bs],training=True)
+                if (ik % 2000 == 0):
+                    print("#", end="")
+            model.evaluate(cifar10.x_val,cifar10.y_val,batch_size=trr.val_bs,verbose=2)
+            model.evaluate(cifar10.x_test,cifar10.y_test,batch_size=trr.val_bs,verbose=2)
