b8babeeec03939a0cdcd7c8a722a73d5e5d8b684
David <2072224025@qq.com>
2024-05-02 17:44:12 +0800

    reorganization

--- execLog.bat
+++
@@ -1,1 +0,0 @@
-powershell "python %1 %2 %3 | tee (\"%1\"+\"-\"+$(Get-Date -Format \"yyyy-MM-dd-HH-mm\")+\".log\")"
---
+++ llama3/binary_llama/__init__.py
@@ -0,0 +1,3 @@
+from .binary_generation import BinaryLlama
+from .binary_model import BinaryModelArgs, BinaryTransformer
+from .binary_functions import kk_schedule_stage1, kk_schedule_stage2
